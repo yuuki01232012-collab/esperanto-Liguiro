9e6f8b4eb209c1542079cc05422374e715ed92cf
yuuki01232012-collab <yuuki01232012@gmail.com>
2026-08-28 05:34:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 script.js
 const toLiguiro = {
-  "a": "ᚨ", "b": "ᛒ", "c": "ᚲ", "ĉ": "ᚲ^",
-  "d": "ᛞ", "e": "ᛊ", "f": "ᚠ", "g": "ᚷ",
-  "ĝ": "ᚷ^", "h": "ᚺ", "ĥ": "ᚺ^", "i": "ᛁ",
-  "j": "ᚼ", "ĵ": "ᚼ^", "k": "K", "l": "L",
-  "m": "ᛗ", "n": "ᚾ", "o": "ᛟ", "p": "ᛈ",
-  "q": "ᚦ", "r": "ᚱ", "s": "ᛋ", "ŝ": "ᛋ^",
-  "t": "ᛏ", "u": "ᚢ", "ŭ": "ᚢ^", "v": "V",
-  "w": "ᚹ", "x": "ᛝ", "y": "ᛃ", "z": "Z",
+  "a": "ᚨ",
+  "b": "ᛒ",
+  "c": "ᚲ",
+  "ĉ": "ᚲ^",
+  "d": "ᛞ",
+  "e": "ᛊ",
+  "f": "ᚠ",
+  "g": "ᚷ",
+  "ĝ": "ᚷ^",
+  "h": "ᚺ",
+  "ĥ": "ᚺ^",
+  "i": "ᛁ",
+  "j": "ᚼ",
+  "ĵ": "ᚼ^",
+  "k": "K",
+  "l": "L",
+  "m": "ᛗ",
+  "n": "ᚾ",
+  "o": "ᛟ",
+  "p": "ᛈ",
+  "q": "ᚦ",
+  "r": "ᚱ",
+  "s": "ᛋ",
+  "ŝ": "ᛋ^",
+  "t": "ᛏ",
+  "u": "ᚢ",
+  "ŭ": "ᚢ^",
+  "v": "V",
+  "w": "ᚹ",
+  "x": "ᛝ",
+  "y": "ᛃ",
+  "z": "Z",
 
-  "A": "ᚨ", "B": "ᛒ", "C": "ᚲ", "Ĉ": "ᚲ^",
-  "D": "ᛞ", "E": "ᛊ", "F": "ᚠ", "G": "ᚷ",
-  "Ĝ": "ᚷ^", "H": "ᚺ", "Ĥ": "ᚺ^", "I": "ᛁ",
-  "J": "ᚼ", "Ĵ": "ᚼ^", "K": "K", "L": "L",
-  "M": "ᛗ", "N": "ᚾ", "O": "ᛟ", "P": "ᛈ",
-  "Q": "ᚦ", "R": "ᚱ", "S": "ᛋ", "Ŝ": "ᛋ^",
-  "T": "ᛏ", "U": "ᚢ", "Ŭ": "ᚢ^", "V": "V",
-  "W": "ᚹ", "X": "ᛝ", "Y": "ᛃ", "Z": "Z"
+  "A": "ᚨ",
+  "B": "ᛒ",
+  "C": "ᚲ",
+  "Ĉ": "ᚲ^",
+  "D": "ᛞ",
+  "E": "ᛊ",
+  "F": "ᚠ",
+  "G": "ᚷ",
+  "Ĝ": "ᚷ^",
+  "H": "ᚺ",
+  "Ĥ": "ᚺ^",
+  "I": "ᛁ",
+  "J": "ᚼ",
+  "Ĵ": "ᚼ^",
+  "K": "K",
+  "L": "L",
+  "M": "ᛗ",
+  "N": "ᚾ",
+  "O": "ᛟ",
+  "P": "ᛈ",
+  "Q": "ᚦ",
+  "R": "ᚱ",
+  "S": "ᛋ",
+  "Ŝ": "ᛋ^",
+  "T": "ᛏ",
+  "U": "ᚢ",
+  "Ŭ": "ᚢ^",
+  "V": "V",
+  "W": "ᚹ",
+  "X": "ᛝ",
+  "Y": "ᛃ",
+  "Z": "Z"
 };
 
 
@@ -59,7 +107,6 @@ const toEsperanto = {
 
 function convertToLiguiro() {
   const input = document.getElementById("esperanto").value;
-
   let result = "";
 
   for (const character of input) {
@@ -67,18 +114,16 @@ function convertToLiguiro() {
   }
 
   document.getElementById("result").textContent =
-    "変換結果：" + (result || "no");
+    "変換結果：" + result;
 }
 
 
 function convertToEsperanto() {
   const input = document.getElementById("liguiro").value;
-
   let result = "";
   let i = 0;
 
   while (i < input.length) {
-
     const two = input.substring(i, i + 2);
 
     if (toEsperanto[two]) {
@@ -90,10 +135,9 @@ function convertToEsperanto() {
     const one = input[i];
 
     result += toEsperanto[one] || one;
-
     i++;
   }
 
   document.getElementById("liguiroResult").textContent =
-    "変換結果：" + (result || "no");
-}
+    "変換結果：" + result;
+    }
